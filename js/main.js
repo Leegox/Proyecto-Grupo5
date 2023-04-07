227bd9de387c42490c8c081cfb582d1c373ce173
Mireva <chichik43@gmail.com>
2023-04-07 19:37:14 -0300

    txt con los colores y ventana de cookies en index

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+/* JS VENTANA EMERGENTE */
+
+var cookiePopup = document.getElementById("cookie-popup");
+var acceptCookies = document.getElementById("accept-cookies");
+
+function hidePopup() {
+    cookiePopup.style.display = "none";
+}
+
+acceptCookies.addEventListener("click", function() {
+    hidePopup();
+});
+
+window.onload = function() {
+    cookiePopup.style.display = "flex";
+};
+
+
+/* FIN JS VENTANA EMERGENTE */
