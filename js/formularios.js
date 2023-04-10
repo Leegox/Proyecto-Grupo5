dd33a65e8da2a26f429f7f6d5326718cc1f795f6
Nazareno Lezcano <nz.lez19@gmail.com>
2023-04-10 05:33:22 -0300

    nuevos formularios

--- js/formularios.js
+++ js/formularios.js
@@ -1,9 +1,5 @@
-$ (document).ready(()=> {
 
-    $ ('.form1'). click (()=>{
-
-    $('.form1').toggle();
-
-    });
-
-});
+function mostrar(){
+    document.getElementById('mostrarForm').style.display='block';
+    
+};
