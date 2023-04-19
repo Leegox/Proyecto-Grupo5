aa660ed9bda6ed26e64d035b2ca36e367224fb56
Nazareno Lezcano <nz.lez19@gmail.com>
2023-04-19 05:44:37 -0300

    FORMULARIOS TERMINADOS

--- js/proceso.js
+++ js/proceso.js
@@ -13,7 +13,7 @@ btnEnviar.addEventListener("click", () => {
   const nombre = document.getElementById('nom').value;
   const apellido = document.getElementById('ap').value;
   const dni = document.getElementById('dni').value;
-  const tel = document.getElementById('tel/cel').value;
+  const tel = document.getElementById('tel-cel').value;
   const direc = document.getElementById('direc').value;
   const email = document.getElementById('e-mail').value;
   const numPedido = document.getElementById('num').value;
@@ -38,33 +38,93 @@ btnEnviar.addEventListener("click", () => {
 });
 
 
-//validacion de form1
-function validar() {
-  // Obtener los valores de los campos
-  const nombre = document.getElementById("nom").value.trim();
-  const apellido = document.getElementById("ap").value.trim();
-  
-  // Validar que los campos no estén vacíos
-  if (nombre === "") {
-    alert("Debe completar el campo: Nombre.");
-    return false;
-  }
-  if (apellido === ""){
-    alert("Debe completar el campo: Apellido:.");
-    return false;
-  }
-  // Validar que los campos sean alfanuméricos
-  const alfanumerico = /^[a-zA-Z0-9]+$/;
-  if (!nombre.match(alfanumerico) || !apellido.match(alfanumerico)) {
-    alert("Los campos solo pueden contener letras.");
-    return false;
-  }
-  
-  // Si se pasan todas las validaciones, enviar el formulario
-  alert("Formulario enviado correctamente.");
-  return true;
-}
+const botonEnviar = document.getElementById('enviar-formulario');
 
+botonEnviar.addEventListener('click', (event) => {
+  if (nombre.value === '' || apellido.value === '' || dni.value === '' || isNaN(dni.value) || telefono.value === '' || isNaN(telefono.value) || direccion.value === '' || email.value === '' || numTransac.value === '' ) {
+    alert('Por favor, complete todos los campos antes de enviar el formulario.');
+    event.preventDefault();
+  } else {
+    alert('¡Tu solicitud fue registrada con éxito!');
+    form.submit();
+  }
+});
+
+
+const form = document.querySelector('form');
+const nombre = document.querySelector('#nom');
+const apellido = document.querySelector('#ap');
+const dni = document.querySelector('#dni');
+const telefono = document.querySelector('#tel-cel');
+const direccion = document.querySelector('#direc');
+const email = document.querySelector('#e-mail');
+const numTransac = document.querySelector('#num');
+const enviarBtn = document.querySelector('#enviar-formulario');
+
+enviarBtn.addEventListener('click', (event) => {
+  event.preventDefault(); // Prevenir el envío del formulario
+
+  // Validar el campo de nombre
+  if (nombre.value === '') {
+    nombre.parentNode.querySelector('span').textContent = 'Campo obligatorio';
+    nombre.parentNode.querySelector('span').style.color = 'red';
+  } else {
+    nombre.parentNode.querySelector('span').textContent = '';
+  }
+
+  // Validar el campo de apellido
+  if (apellido.value === '') {
+    apellido.parentNode.querySelector('span').textContent = 'Campo obligatorio';
+    apellido.parentNode.querySelector('span').style.color = 'red';
+  } else {
+    apellido.parentNode.querySelector('span').textContent = '';
+  }
+
+  // Validar el campo de edad
+  if (dni.value === '' || isNaN(dni.value)) {
+    dni.parentNode.querySelector('span').textContent = 'Por favor, ingrese su DNI/Pasaporte correctamente';
+    dni.parentNode.querySelector('span').style.color = 'red';
+  } else {
+    dni.parentNode.querySelector('span').textContent = '';
+  }
+
+  // Validar el campo de teléfono
+  if (telefono.value === '' || isNaN(telefono.value)) {
+    telefono.parentNode.querySelector('span').textContent = 'Por favor, ingrese su número de teléfono correctamente';
+    telefono.parentNode.querySelector('span').style.color = 'red';
+  } else {
+    telefono.parentNode.querySelector('span').textContent = '';
+  }
+
+  // Validar el campo de direccion
+  if (direccion.value === '' || isNaN(direccion.value)) {
+    direccion.parentNode.querySelector('span').textContent = 'Por favor, ingrese su dirección correctamente';
+  direccion.parentNode.querySelector('span').style.color = 'red';
+  } else {
+    direccion.parentNode.querySelector('span').textContent = '';
+  }
+
+  // Validar el campo de correo electrónico
+  if (email.value === '') {
+    email.parentNode.querySelector('span').textContent = 'Campo obligatorio';
+    email.parentNode.querySelector('span').style.color = 'red';
+  } else {
+    email.parentNode.querySelector('span').textContent = '';
+  }
+
+   // Validar el campo de Numero de Transacción
+   if (numTransac.value === '' || isNaN(numTransac.value)) {
+    numTransac.parentNode.querySelector('span').textContent = 'Por favor, ingrese su número de Transacción correctamente';
+    numTransac.parentNode.querySelector('span').style.color = 'red';
+  } else {
+  numTransac.parentNode.querySelector('span').textContent = '';
+  }
+
+  // Verificar si todos los campos están completos
+  if (nombre.value !== '' && apellido.value !== '' && !isNaN(dni.value) && !isNaN(telefono.value) && direccion.value !=='' && email.value !== '' && numTransac.value !== '') {
+    form.submit(); // Enviar el formulario
+  }
+});
 
 
 // Variable global para contar el número de objetos
@@ -78,7 +138,7 @@ function guardarDatos() {
   var nombre = document.getElementById("nom").value;
   var apellido = document.getElementById("ap").value;
   var dni = document.getElementById('dni').value;
-  var tel = document.getElementById('tel/cel').value;
+  var tel = document.getElementById('tel-cel').value;
   var direc = document.getElementById('direc').value;
   var email = document.getElementById('e-mail').value;
   var numPedido = document.getElementById('num').value;
